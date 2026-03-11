07c66786b0fa9b28af112dec00ce87072e0b9e87
Urkaz <mrurkaz@gmail.com>
2026-03-11 10:09:47 +0100

    chore: Update license year

--- fabric/src/main/java/com/urkaz/urkazlib/fabric/platform/IPlatformImpl.java
+++ fabric/src/main/java/com/urkaz/urkazlib/fabric/platform/IPlatformImpl.java
@@ -1,5 +1,5 @@
 /*
- * UrkazLib (C) 2025 by Urkaz - Fran Sanchez is licensed under Creative Commons Attribution-NonCommercial 4.0 International.
+ * UrkazLib (C) 2026 by Urkaz - Fran Sanchez is licensed under Creative Commons Attribution-NonCommercial 4.0 International.
  * To view a copy of this license, visit https://creativecommons.org/licenses/by-nc/4.0/
  */
 
